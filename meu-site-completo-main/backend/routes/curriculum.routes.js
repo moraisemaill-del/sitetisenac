b6ce26e957d24f53851756d28c58248553642df5
moraisemailldel <moraisemaill@gmail.com>
2026-07-30 15:13:57 -0400

    organização das pastas

--- meu-site-completo-main/backend/routes/curriculum.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middleware/auth');
-const { getCurriculum, updateCurriculum } = require('../controllers/curriculum.controller');
-
-router.get('/', getCurriculum);
-router.put('/', auth, updateCurriculum);
-
-module.exports = router;
